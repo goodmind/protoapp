46eb4492d55e5410eb48463c6a000ed7b127cbaf
goodmind <andwebar@gmail.com>
2016-08-16 00:45:38 +0500

    refactor(helpers/generateImports): move from blueprints

---
+++ src/helpers/generateImports.ts
@@ -0,0 +1,21 @@
+import { ProtobufService, ProtobufMessage } from '../interfaces'
+import { flatten } from './utils'
+
+export function generateImportsMessage (model: ProtobufMessage, primitives: string[]): string[] {
+  return model.fields
+    .map(x => x.type)
+    .filter(x => primitives.indexOf(x) < 0)
+    .filter(x => x !== model.name)
+}
+
+export function generateImportsService (model: ProtobufService, primitives: string[]): string[] {
+  let rpc = model.rpc
+  let refs = Object.keys(rpc)
+    .map(x => [rpc[x].request, rpc[x].response])
+
+  let types = flatten(refs)
+    .filter((x: any) => primitives.indexOf(x) < 0)
+    .filter((x: any) => x !== model.name)
+
+  return Array.from(new Set(types))
+}
