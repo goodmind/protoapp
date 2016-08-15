f1a2fc56dae12a1d2657f680bee46c0ac4337aa9
goodmind <andwebar@gmail.com>
2016-08-16 00:49:53 +0500

    refactor(helpers/utils): move from cli/protoapp

---
+++ src/helpers/utils.ts
@@ -0,0 +1,24 @@
+import * as mkdirp from 'mkdirp'
+import * as path from 'path'
+import * as fs from 'fs'
+
+export function flatten (arr: any[]): any[] {
+  return [].concat.apply([], arr)
+}
+
+export function writeFile (filename: string, data: any): Promise<any> {
+  return createDirectory(path.dirname(filename))
+    .then(x => new Promise((resolve, reject) => {
+      fs.writeFile(filename, data, 'UTF-8', (err) => {
+        if (err) { reject(err) } else { resolve(data) }
+      })
+    }))
+}
+
+export function createDirectory (path: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    mkdirp(path, (err, made) => {
+      if (err) { reject(err) } else { resolve(made) }
+    })
+  })
+}
