1b43c6430ac7acd018398d59d9baea1c43b9b81d
goodmind <andwebar@gmail.com>
2016-08-24 11:23:25 +0500

    refactor(transformation/plugin): add `PluginContext` type

--- src/transformation/plugin.ts
+++ src/transformation/plugin.ts
@@ -1,17 +1,33 @@
 import { resolve } from '../helpers/resolve'
 
-interface PluginModule {
+export interface PluginContext {
+  dirname?: string
+  generateImportsService?: Function
+  generateImportsMessage?: Function
+  primitiveTypes?: string[]
+  dustTemplate?: Function
+}
+
+export interface PluginModule {
   __esModule: any
   default: any
 }
 
-interface Plugin {}
+export interface Plugin {}
 
 function isPluginModule (plugin: PluginModule|Plugin): plugin is PluginModule {
   return (plugin as PluginModule).__esModule
 }
 
-export function normalisePlugin (plugin: string|PluginModule|Plugin, dirname: string, context: any = {}): any {
+export function normalisePlugin (
+  plugin: string|PluginModule|Plugin,
+  dirname: string,
+  context: PluginContext = {}
+): any {
+  if (!plugin) {
+    return null
+  }
+
   if (typeof plugin === 'string') {
     let pluginLoc = resolve(`protoapp-plugin-${plugin}`, dirname) || resolve(plugin, dirname)
     if (pluginLoc) {
